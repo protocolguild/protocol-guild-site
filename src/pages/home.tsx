b18b45403871b807a84e0a87de3b8f72b096b989
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-17 11:37:55 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -185,14 +185,14 @@ const Home: FC = () => {
               <DuneStat 
                 queryId="4876407" 
                 theme="light" 
-                title="Median Distributed per Member Last 12 Months"
+                title="Median Distributed per Dev Last 12 Months"
                 variableName="yr_median_vested_hist"
                 type="currency"
               />
               <DuneStat 
                 queryId="4872646" 
                 theme="light" 
-                title="Median Vesting per Member Next 12 Months"
+                title="Expected Median Distribution per Dev Next 12 Months"
                 variableName="median_vesting_365_days"
                 type="currency"
               />
