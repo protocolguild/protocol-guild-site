4d06a64136f77f1a13196d4be8606dc7930c22d2
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-17 10:21:40 +0200

    Update DonorSectionDark.tsx

--- src/components/DonorSectionDark.tsx
+++ src/components/DonorSectionDark.tsx
@@ -1,5 +1,5 @@
 // Component imports
-import pgIcon from '../assets/images/pg-icon.svg'
+import pgIcon from '../assets/images/pg-icon-dark.png'
 // 1% Partners
 import etherfiSvg from '../assets/images/donors/etherfi.svg'
 import etherfiWhiteSvg from '../assets/images/donors/etherfi-white.svg'
