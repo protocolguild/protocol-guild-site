e62de0eeebdd32d05cea09eff8b756ff2be7c33a
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-17 12:06:36 +0200

    Update navigation.ts

--- src/content/navigation.ts
+++ src/content/navigation.ts
@@ -1,13 +1,12 @@
 export const navigation = {
   main: [
     { name: 'HOME', path: '/' },
-    { name: 'PLEDGE', path: '/pledge' },
     { name: 'ABOUT', path: '/about' },
     { name: 'DOCS', path: 'https://protocol-guild.readthedocs.io/en/latest/index.html', target: '_blank' }
   ],
   footer: {
     contact: {
-      email: "trenton.vanepps@ethereum.org",
+      email: "protocolguild@gmail.com",
       twitter: "https://x.com/ProtocolGuild"
     }
   }
