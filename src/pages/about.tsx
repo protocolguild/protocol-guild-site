28ac289eed64d11661f6027c9ed8d965dc893464
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-17 17:35:42 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -96,51 +96,10 @@ const About: FC = () => {
         <Section.Row align="start">
           <Grid columns={12} className="py-16">
             <Grid.Item span={6} className="flex flex-col gap-6">
-              <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{aboutContent.funding.title}</h2>
+              <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{aboutContent.section2.title}</h2>
+              <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.section2.subtitle}</h2>
             </Grid.Item>
             <Grid.Item span={6} className="flex flex-col gap-6">
-              <div className="w-full flex flex-row justify-between items-center">
-                <p className="text-[var(--gray-dark)] text-base sm:text-sm">{aboutContent.funding.statsHeader}</p>
-                <a href="https://dune.com/protocolguild/protocol-guild" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 text-[var(--gray-dark)] text-base sm:text-sm ">
-                  View on Dune
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
-                </a>
-              </div>
-              <DuneStat 
-                queryId="4872646" 
-                theme="light" 
-                title="Median Vesting per Member over Next 12 Months"
-                variableName="median_vesting_365_days"
-                type="currency"
-              />
-              <DuneStat 
-                queryId="4876407" 
-                theme="light" 
-                title="Total Amount Distributed by Protocol Guild"
-                variableName="total_vested_hist"
-                type="currency"
-              />
-              <DuneChart 
-                  queryId="3851797" 
-                  theme="light" 
-                  title="Vesting Forecast"
-                  xAxisKey="date"
-                  yAxisKey="total_vesting"
-                  formatCurrency
-                  formatDate
-                />
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <FeatureCard
-                  title="Transparent Finance"
-                  description="Funding is fully onchain and fully auditable. Review our smart contracts and funding history."
-                  color="var(--gray-dark)"
-                />
-                <FeatureCard
-                  title="Self-curation"
-                  description="Members of Protocol Guild participate in real-time adjustment of who's eligible for Protocol Guild rewards."
-                  color="var(--gray-dark)"
-                />
-              </div>
             </Grid.Item>
           </Grid>
         </Section.Row>
@@ -183,18 +142,6 @@ const About: FC = () => {
                   formatCurrency
                   formatDate
                 />
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                <FeatureCard
-                  title="Transparent Finance"
-                  description="Funding is fully onchain and fully auditable. Review our smart contracts and funding history."
-                  color="var(--gray-dark)"
-                />
-                <FeatureCard
-                  title="Self-curation"
-                  description="Members of Protocol Guild participate in real-time adjustment of who's eligible for Protocol Guild rewards."
-                  color="var(--gray-dark)"
-                />
-              </div>
             </Grid.Item>
           </Grid>
         </Section.Row>
