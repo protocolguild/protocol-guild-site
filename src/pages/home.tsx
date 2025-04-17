f37fb32eadfa70a6206360559ec420d60530ff1e
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-17 10:03:28 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -56,10 +56,10 @@ const Home: FC = () => {
             <Grid.Item span={6} className="flex flex-col gap-6">
               <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{homeContent.impact.title}</h2>
               <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">{homeContent.impact.subtitle1}</h2>
-              <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">{homeContent.impact.subtitle1}</h2>
+              <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">{homeContent.impact.subtitle2}</h2>
               <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">
                 <a href="https://tim.mirror.xyz/srVdVopOFhD_ZoRDR50x8n5wmW3aRJIrNEAkpyQ4_ng" target="_blank" rel="noopener noreferrer">
-                  {homeContent.impact.subtitle1}
+                  {homeContent.impact.subtitle3}
                 </a>
               </h2>
             </Grid.Item>
