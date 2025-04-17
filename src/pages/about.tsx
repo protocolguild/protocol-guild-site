c2eac9f628106a2e06666a29300dfb9da3f42edf
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-17 20:54:48 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -159,29 +159,6 @@
                 <DuneStat 
                   queryId="2478156" 
                   theme="light" 
-                  title="Median Donation"
-                  variableName="median_donation"
-                  type="currency"
-                />
-                <DuneStat 
-                  queryId="2478156" 
-                  theme="light" 
-                  title="Average Donation"
-                  variableName="avg_donation"
-                  type="currency"
-                />
-              </Grid.Item>
-              <Grid.Item span={6} className="flex flex-col gap-6">
-                <div className="w-full flex flex-row justify-between items-center">
-                  <p className="text-[var(--gray-dark)] text-base sm:text-sm"></p>
-                  <a href="https://dune.com/protocolguild/protocol-guild" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 text-[var(--gray-dark)] text-base sm:text-sm ">
-                    View on Dune
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
-                  </a>
-                </div>
-                <DuneStat 
-                  queryId="2478156" 
-                  theme="light" 
                   title="Total Donated - Initial value"
                   variableName="total_historical_value"
                   type="currency"
@@ -193,6 +170,15 @@
                   variableName="total_current_value"
                   type="currency"
                 />
+              </Grid.Item>
+              <Grid.Item span={6} className="flex flex-col gap-6">
+                <div className="w-full flex flex-row justify-between items-center">
+                  <p className="text-[var(--gray-dark)] text-base sm:text-sm"></p>
+                  <a href="https://dune.com/protocolguild/protocol-guild" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 text-[var(--gray-dark)] text-base sm:text-sm ">
+                    View on Dune
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+                  </a>
+                </div>
                 <DuneStat 
                   queryId="4872646" 
                   theme="light" 
