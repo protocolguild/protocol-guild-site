0d7e5ffb7b82c9d3cb646f4db994d8264a874990
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-22 11:29:28 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Layout from './components/Layout'
 import Home from './pages/home'
-import Pledge from './pages/pledge'
+import Donate from './pages/donaate'
 import About from './pages/about'
 import ScrollToTop from './components/ScrollToTop'
 
@@ -14,7 +14,7 @@ const App: FC = () => {
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/pledge" element={<Pledge />} />
+          <Route path="/donate" element={<Donate />} />
           <Route path="/about" element={<About />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
