1834a89f1e2868c1c67793911f8dae33fb73a031
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-22 11:21:26 +0200

    Update navigation.ts

--- src/content/navigation.ts
+++ src/content/navigation.ts
@@ -1,8 +1,8 @@
 export const navigation = {
   main: [
     { name: 'HOME', path: '/' },
+    { name: 'DONATE', path: '/donate' },
     { name: 'ABOUT', path: '/about' },
-    { name: 'CONTACT', path: 'https://calendly.com/vecchiarelli-peter/30min', target: '_blank' },
     { name: 'DOCS', path: 'https://protocol-guild.readthedocs.io/en/latest/index.html', target: '_blank' }
   ],
   footer: {
