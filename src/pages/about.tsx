d57053d56701f0ac3dd2d217f05a8d49d38aee81
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-22 16:19:39 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -58,7 +58,7 @@
                 <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.organization.description}</h2>
               </Grid.Item>
               <Grid.Item span={6} className="flex flex-col gap-4">
-                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                <div className="grid grid-cols-1 gap-6">
                   <FeatureCard
                     title="Protocol Guild Members"
                     description="Over 190 researchers and developers are members of Protocol Guild"
@@ -83,7 +83,7 @@
                     </a>
                   </h2>
                 </div>
-                <br/>
+                <br />
                 <div>
                   <h3 className="text-lg font-bold leading-[1.1] text-[var(--gray-dark)] mb-4">{aboutContent.organization.team}</h3>
                   <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
@@ -91,7 +91,7 @@
                       <img src={trentImg} alt="Trent Van Epps" className="w-full aspect-square object-cover" />
                       <div className="mt-2 flex flex-col items-start w-full gap-1">
                         <p className="text-sm font-bold">Trent Van Epps</p>
-                        <a href="https://x.com/trent_vanepps" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)]">@trent_vanepps
+                        <a href="https://x.com/trent_vanepps" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)] text-sm font-normal">@trent_vanepps
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block ml-1">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                             <polyline points="15 3 21 3 21 9"></polyline>
@@ -106,7 +106,7 @@
                       <img src={cheekyImg} alt="Cheeky Gorilla" className="w-full aspect-square object-cover" />
                       <div className="mt-2 flex flex-col items-start w-full gap-1">
                         <p className="text-sm font-bold">Cheeky Gorilla</p>
-                        <a href="https://x.com/cheekygorilla0x" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)]">@cheekygorilla0x
+                        <a href="https://x.com/cheekygorilla0x" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)] text-sm font-normal">@cheekygorilla0x
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block ml-1">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                             <polyline points="15 3 21 3 21 9"></polyline>
@@ -121,7 +121,7 @@
                       <img src={peterImg} alt="Peter Vecchiarelli" className="w-full aspect-square object-cover" />
                       <div className="mt-2 flex flex-col items-start w-full gap-1">
                         <p className="text-sm font-bold">Peter Vecchiarelli</p>
-                        <a href="https://x.com/AlphaLemonade" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)]">@AlphaLemonade
+                        <a href="https://x.com/AlphaLemonade" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)] text-sm font-normal">@AlphaLemonade
                           <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block ml-1">
                             <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                             <polyline points="15 3 21 3 21 9"></polyline>
@@ -137,7 +137,8 @@
               </Grid.Item>
             </Grid>
           </Section.Row>
-        </Section>  
+        </Section>
+      
         <Section background="white" divider="var(--brand-primary)" line={true}>
           <Section.Row align="start">
             <Grid columns={12} className="py-16">
