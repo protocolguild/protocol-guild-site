7c1b6297ef62e03b9c4204f2af16292161ec0c9e
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-22 19:51:32 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -142,13 +142,13 @@ const About: FC = () => {
             <Grid.Item span={6} className="flex flex-col gap-6 md:h-full">
               <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{aboutContent.impactAreas.title}</h2>
               <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.impactAreas.subtitle}</h2>
-              <DuneChart 
+              <DuneStat 
                 queryId="5021583" 
                 theme="light" 
                 title="Cumulative Years Contributed"
                 suffix="years"
                 variableName="time"
-                type="number"
+                type="currency"
               />
             </Grid.Item>
             <Grid.Item span={6} className="flex flex-col gap-6">
