72bd70f5a0260ac264307e534aab649183a1dd70
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-22 17:21:50 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -206,10 +206,10 @@ const About: FC = () => {
           <Section.Row align="start">
             <Grid columns={12} className="py-16">
               <Grid.Item span={6} className="flex flex-col gap-4">
-                <h2 className="col-span-3 text-3xl font-bold leading-[1.1] text-[var(--white)] mb-4">
+                <h2 className="col-span-3 text-3xl font-bold leading-[1.1] text-[var(--gray-dark)] mb-4">
                   {aboutContent.docs.title}
                 </h2>
-                <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--white)]">{aboutContent.docs.description}</h2>
+                <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.docs.description}</h2>
                 <a href="https://protocol-guild.readthedocs.io/en/latest/index.html" target="_blank" rel="noopener noreferrer" className="!bg-[var(--brand-primary)] !border-none text-[var(--gray-dark)] hover:text-[var(--gray-dark)] !hover:text-[var(--gray-dark)] px-8 py-3 rounded-lg hover:!opacity-80 transition-colors w-fit inline-block no-underline">View Documentation</a>
               </Grid.Item>
             </Grid>
