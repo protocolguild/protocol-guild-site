7308b7b83c2a5d2af4e2b5d92efebebcf4674a07
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-22 12:46:35 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -13,7 +13,6 @@ import { homeContent } from '../content/home'
 import DuneStat from '../components/DuneStat'
 import DuneChart from '../components/DuneChart'
 
-
 const Home: FC = () => {
 
   const [currentQuote, setCurrentQuote] = useState(0);
@@ -119,7 +118,7 @@ const Home: FC = () => {
               <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)] flex items-center">
                 <a href="https://protocol-guild.readthedocs.io/en/latest/02-membership.html" target="_blank" rel="noopener noreferrer" className="flex items-center">
                   {homeContent.section2.link}
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="ml-1"> {/* Add margin to the left for spacing */}
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="ml-1">
                     <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
                     <polyline points="15 3 21 3 21 9"></polyline>
                     <line x1="10" y1="14" x2="21" y2="3"></line>
