20750c34483c25d9cdf8abdacd068eb3b778485a
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-22 19:59:23 +0200

    Update home.ts

--- src/content/home.ts
+++ src/content/home.ts
@@ -22,6 +22,6 @@ export const homeContent = {
   section3: {
     title: "Funded by builders from all across the Ethereum ecosystem.",
     subtitle1: "We partner with leading and emerging crypto projects to ensure that our shared infrastructure stays stable and competitive for years to come.",
-    buttonLink: "/pledge"
+    buttonLink: "/donate"
   }
 }
