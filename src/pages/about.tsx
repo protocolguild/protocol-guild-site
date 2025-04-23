555b58e07bf0b0799d614c1de42f79443809ef79
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-23 12:44:44 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -183,16 +183,16 @@ return (
       <Section.Row align="start">
         <Grid columns={12} className="py-16">
           <Grid.Item span={6} className="flex flex-col gap-6">
-            <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{homeContent.stats.title}</h2>
-            <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{homeContent.stats.description1}</h2>
-            <h2 className="!text-lg mt-0 !font-bold !leading-[24px] !text-[var(--gray-dark)]">{homeContent.stats.description2}</h2>
-            <h2 className="!text-lg mt-0 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{homeContent.stats.description3}</h2>
-            <h2 className="!text-lg mt-0 !font-bold !leading-[24px] !text-[var(--gray-dark)]">{homeContent.stats.description4}</h2>
-            <h2 className="!text-lg mt-0 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{homeContent.stats.description5}</h2>
+            <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{aboutContent.stats.title}</h2>
+            <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.stats.description1}</h2>
+            <h2 className="!text-lg mt-0 !font-bold !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.stats.description2}</h2>
+            <h2 className="!text-lg mt-0 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.stats.description3}</h2>
+            <h2 className="!text-lg mt-0 !font-bold !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.stats.description4}</h2>
+            <h2 className="!text-lg mt-0 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.stats.description5}</h2>
           </Grid.Item>
           <Grid.Item span={6} className="flex flex-col gap-6">
             <div className="w-full flex flex-row justify-between items-center">
-              <p className="text-[var(--gray-dark)] text-base sm:text-sm">{homeContent.stats.statsHeader}</p>
+              <p className="text-[var(--gray-dark)] text-base sm:text-sm">{aboutContent.stats.statsHeader}</p>
               <a href="https://dune.com/protocolguild/protocol-guild" target="_blank" rel="noopener noreferrer" className="flex items-center gap-2 text-[var(--gray-dark)] text-base sm:text-sm ">
                 View on Dune
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
