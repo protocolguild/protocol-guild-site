1f7ededa7cefedbc21d95fdd7548e68316a7fa34
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-23 12:25:13 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -95,7 +95,32 @@
         </Section.Row>
       </Section>
   
-
+      <Section background="white" divider="var(--brand-primary)" line={true}>
+        <Section.Row align="start" >
+          <Grid columns={12} className="py-16">
+            <Grid.Item span={6} className="flex flex-col gap-6">
+              <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{aboutContent.moonshot.title}</h2>
+              <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.moonshot.subtitle1}</h2>
+              <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.moonshot.subtitle2}</h2>
+              <h2 className="!text-lg font-bold !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.moonshot.subtitle3}</h2>
+            </Grid.Item>
+            <Grid.Item span={6} className="flex flex-col gap-6">
+              <MemberFaces />
+              <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)] flex items-center">
+                <a href="https://protocol-guild.readthedocs.io/en/latest/02-membership.html" target="_blank" rel="noopener noreferrer" className="flex items-center">
+                  {aboutContent.moonshot.link}
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="ml-1">
+                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                    <polyline points="15 3 21 3 21 9"></polyline>
+                    <line x1="10" y1="14" x2="21" y2="3"></line>
+                  </svg>
+                </a>
+              </h2>
+              <Repos />
+            </Grid.Item>
+          </Grid>
+        </Section.Row>
+      </Section>
 
 
 
