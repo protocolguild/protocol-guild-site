8fc3fd1af1c3b319afd04d06211370ea01acc393
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-23 09:49:41 +0200

    Update DonorSectionDark4.tsx

--- src/components/DonorSectionDark4.tsx
+++ src/components/DonorSectionDark4.tsx
@@ -13,48 +13,15 @@ import pufferSvg from '../assets/images/donors/puffer.svg'
 import pufferWhiteSvg from '../assets/images/donors/puffer-white.svg'
 
 // Donors
-import layerzeroSvg from '../assets/images/donors/layerzero.svg'
-import layerzeroWhiteSvg from '../assets/images/donors/layerzero-white.svg'
-import starknetSvg from '../assets/images/donors/starknet.svg'
-import starknetWhiteSvg from '../assets/images/donors/starknet-white.svg'
-import ensSvg from '../assets/images/donors/ens.svg'
-import ensWhiteSvg from '../assets/images/donors/ens-white.svg'
-import optimismSvg from '../assets/images/donors/optimism.svg'
-import optimismWhiteSvg from '../assets/images/donors/optimism-white.svg'
-import zksyncSvg from '../assets/images/donors/zksync.svg'
-import zksyncWhiteSvg from '../assets/images/donors/zksync-white.svg'
-import scrollSvg from '../assets/images/donors/scroll.svg'
-import scrollWhiteSvg from '../assets/images/donors/scroll-white.svg'
-import octantSvg from '../assets/images/donors/octant.svg'
-import octantWhiteSvg from '../assets/images/donors/octant-white.svg'
-import safeSvg from '../assets/images/donors/safe.svg'
-import safeWhiteSvg from '../assets/images/donors/safe-white.svg'
-import bitwiseSvg from '../assets/images/donors/bitwise.svg'
-import bitwiseWhiteSvg from '../assets/images/donors/bitwise-white.svg'
-import gitcoinSvg from '../assets/images/donors/gitcoin.svg'
-import gitcoinWhiteSvg from '../assets/images/donors/gitcoin-white.svg'
-import arbitrumSvg from '../assets/images/donors/arbitrum.svg'
-import arbitrumWhiteSvg from '../assets/images/donors/arbitrum-white.svg'
-import lidoSvg from '../assets/images/donors/lido.svg'
-import lidoWhiteSvg from '../assets/images/donors/lido-white.svg'
-import uniswapSvg from '../assets/images/donors/uniswap.svg'
-import uniswapWhiteSvg from '../assets/images/donors/uniswap-white.svg'
-import molochSvg from '../assets/images/donors/molochdao.svg'
-import molochWhiteSvg from '../assets/images/donors/molochdao-white.svg'
-import nounsSvg from '../assets/images/donors/nouns.svg'
-import nounsWhiteSvg from '../assets/images/donors/nouns-white.svg'
-import lambdaClassSvg from '../assets/images/donors/lambdaclass.svg'
-import lambdaClassWhiteSvg from '../assets/images/donors/lambdaclass-white.svg'
-import tetranodeSvg from '../assets/images/donors/tetranode.svg'
-import tetranodeWhiteSvg from '../assets/images/donors/tetranode-white.svg'
-import anthonySassanoSvg from '../assets/images/donors/anthonysassano.svg'
-import anthonySassanoWhiteSvg from '../assets/images/donors/anthonysassano-white.svg'
-import dripsSvg from '../assets/images/donors/drips.svg'
-import dripsWhiteSvg from '../assets/images/donors/drips-white.svg'
-import statefulWorksSvg from '../assets/images/donors/statefulworks.svg'
-import statefulWorksWhiteSvg from '../assets/images/donors/statefulworks-white.svg'
-import vanEckSvg from '../assets/images/donors/vaneck.svg'
-import vanEckWhiteSvg from '../assets/images/donors/vaneck-white.svg'
+import baseLogo from '../assets/images/base.svg'
+import optimismLogo from '../assets/images/optimism.svg'
+import polygonLogo from '../assets/images/polygon.svg'
+import realLogo from '../assets/images/real.svg'
+import scrollLogo from '../assets/images/scroll.svg'
+import shapeLogo from '../assets/images/shape.svg'
+import zksyncLogo from '../assets/images/zksync.svg'
+import zoraLogo from '../assets/images/zora.svg'
+import arbitrumLogo from '../assets/images/arbitrum.svg'
 
 /*
 
@@ -65,31 +32,15 @@ const partners = [
 ]
 
 const donors = [
-  { name: 'EigenLayer', logo: eigenlayerSvg, logoWhite: eigenlayerWhiteSvg, url: 'https://eigenlayer.xyz' },
-  { name: 'Taiko', logo: taikoSvg, logoWhite: taikoWhiteSvg, url: 'https://taiko.xyz' },
-  { name: 'Ether.fi', logo: etherfiSvg, logoWhite: etherfiWhiteSvg, url: 'https://ether.fi' },
-  { name: 'LayerZero', logo: layerzeroSvg, logoWhite: layerzeroWhiteSvg, url: 'https://layerzero.network' },
-  { name: 'Puffer', logo: pufferSvg, logoWhite: pufferWhiteSvg, url: 'https://puffer.fi' },
-  { name: 'Optimism', logo: optimismSvg, logoWhite: optimismWhiteSvg, url: 'https://optimism.io' },
-  { name: 'ZKSync', logo: zksyncSvg, logoWhite: zksyncWhiteSvg, url: 'https://zksync.io' },
-  { name: 'Scroll', logo: scrollSvg, logoWhite: scrollWhiteSvg, url: 'https://scroll.io' },
-  { name: 'Starknet', logo: starknetSvg, logoWhite: starknetWhiteSvg, url: 'https://starknet.io' },
-  { name: 'ENS', logo: ensSvg, logoWhite: ensWhiteSvg, url: 'https://ens.domains' },
-  { name: 'Octant', logo: octantSvg, logoWhite: octantWhiteSvg, url: 'https://octant.io' },
-  { name: 'SAFE DAO', logo: safeSvg, logoWhite: safeWhiteSvg, url: 'https://docs.safe.global/safenet/protocol/safe-dao' },
-  { name: 'Bitwise', logo: bitwiseSvg, logoWhite: bitwiseWhiteSvg, url: 'https://bitwiseinvestments.com/' },
-  { name: 'Gitcoin', logo: gitcoinSvg, logoWhite: gitcoinWhiteSvg, url: 'https://gitcoin.co' },
-  { name: 'Arbitrum', logo: arbitrumSvg, logoWhite: arbitrumWhiteSvg, url: 'https://arbitrum.io/community' },
-  { name: 'Lido', logo: lidoSvg, logoWhite: lidoWhiteSvg, url: 'https://lido.fi' },
-  { name: 'Uniswap', logo: uniswapSvg, logoWhite: uniswapWhiteSvg, url: 'https://uniswap.org' },
-  { name: 'MolochDAO', logo: molochSvg, logoWhite: molochWhiteSvg, url: 'https://molochdao.com' },
-  { name: 'NounsDAO', logo: nounsSvg, logoWhite: nounsWhiteSvg, url: 'https://nouns.wtf' },
-  { name: 'Lambda Class', logo: lambdaClassSvg, logoWhite: lambdaClassWhiteSvg, url: 'https://lambdaclass.com' },
-  { name: 'Tetranode', logo: tetranodeSvg, logoWhite: tetranodeWhiteSvg, url: 'https://x.com/Tetranode' },
-  { name: 'Anthony Sassano', logo: anthonySassanoSvg, logoWhite: anthonySassanoWhiteSvg, url: 'https://x.com/sassal0x' },
-  { name: 'Drips', logo: dripsSvg, logoWhite: dripsWhiteSvg, url: 'https://drips.network' },
-  { name: 'Stateful Works', logo: statefulWorksSvg, logoWhite: statefulWorksWhiteSvg, url: 'https://x.com/StatefulWorks' },
-  { name: 'VanEck', logo: vanEckSvg, logoWhite: vanEckWhiteSvg, url: 'https://www.vaneck.com/us/en/insights/digital-assets/' }
+  { name: 'Arbitrum', logo: arbitrumLogo },
+  { name: 'Base', logo: baseLogo },
+  { name: 'Optimism', logo: optimismLogo },
+  { name: 'Polygon', logo: polygonLogo },
+  { name: 're.al', logo: realLogo },
+  { name: 'Scroll', logo: scrollLogo },
+  { name: 'Shape', logo: shapeLogo },
+  { name: 'zkSync', logo: zksyncLogo },
+  { name: 'Zora', logo: zoraLogo },
 ]
 
 
