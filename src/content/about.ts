df1a267be2d6af39417f4df237d6185c21386634
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-23 12:20:01 +0200

    Update about.ts

--- src/content/about.ts
+++ src/content/about.ts
@@ -3,7 +3,7 @@ export const aboutContent = {
     title: "Securing the future of Ethereum core development",
     subtitle: "Protocol Guild is the leading independent organization dedicated to sustainable funding for Ethereum’s core protocol development."
   },
-  section1: {
+  distribution: {
     title: "Since 2022, we have distributed $28M directly to Ethereum core developers.",
     quote: {
       text: "\"I love that ProtocolGuild centers individual contributors ahead of organizations, and in doing so grants autonomy to all of the people working on Ethereum's base layer.\"",
@@ -11,19 +11,56 @@ export const aboutContent = {
     },
     statsHeader: "Cumulative Amount Distributed"
   },
-  section2: {
+  stewards: {
     title: "Supporting the stewards of Ethereum’s core protocol",
     subtitle: "Every quarter, we curate and update an onchain registry of Ethereum’s active core protocol contributors, to distribute funding towards.",
     link: "View all core devs & researchers"
   },
-  section3: {
-    title: "Funded by projects building on Ethereum",
-    tableHeader: "Donor Leaderboard by Currently Vesting"
+  moonshot: {
+    title: "Our moonshot: Ethereum R&D, funded forever.",
+    subtitle1: "Our ecosystem depends on open source infrastructure. There's still a lot to do to deliver Ethereum as globally ubiquitous infra.",
+    subtitle2: "To allow our ecosystem to fund this work, we make the entire set of Ethereum’s core protocol contributors directly legible onchain, by maintaining an up-to-date registry of Ethereum’s active core protocol contributors. We receive tokens from projects building on Ethereum and distribute them directly to these individuals, vested over four years.",
+    subtitle3: "189 contributors, 47 core repositories, 1 Ethereum protocol.",
+    link: "See all individuals"
   },
-  funding: {
-    title: "Transparent and effective funding"
+  funders: {
+    title: "Funded by builders from all across the Ethereum ecosystem.",
+    subtitle1: "We partner with leading and emerging crypto projects to ensure that our shared infrastructure stays stable and competitive for years to come.",
+    buttonLink: "/donate"
   },
-  organization: {
+  impact: {
+    title: "Our impact areas.",
+    subtitle: "Protocol Guild is the only org that comprehensively funds contributors from across all of Ethereum's core development.",
+    areas: [
+      {
+        title: "Research",
+        description: "R&D to improve Ethereum's security, scalability, and efficiency."
+      },
+      {
+        title: "EIPs",
+        description: "Chain upgrades and protocol hard forks."
+      },
+      {
+        title: "Maintenance",
+        description: "Critical bug fixes, security patches, and network upkeep."
+      },
+      {
+        title: "Development",
+        description: "New features and infrastructure improvements for the Ethereum network."
+      }
+    ],
+    link: "Learn about eligibility"
+  },
+  stats: {
+    title: "Effective and transparent funding.",
+    description1: "We aim to create the most credible and transparent model for rewarding open source contributions. Funding is fully onchain and fully auditable.",
+    description2: "Neutral Distribution",
+    description3: "We dont make discretionary funding allocations - all tokens are streamed over 4 years via immutable contracts, and are allocated based on a simple time-weighting system that incentivizes long-term, full-time contributions.",
+    description4: "Quarterly Curation",
+    description5: "We maintain an up-to-date registry of Ethereum’s active core protocol contributors, updated quarterly, ensuring that funding only goes to the individuals who are actively building and maintaining Ethereum's core protocol.",
+    statsHeader: "Distribution Stats"
+  },
+  org: {
     title: "About the organization",
     description: "Protocol Guild is structured as a non-profit that serves hundreds of Ethereum core protocol contributors. If you have any questions about supporting our mission, please reach out!",
     team: {
