74244abaeb2bb97154d74ecd8fbc1ff40f40259e
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-23 10:11:10 +0200

    Update DonorSectionDark4.tsx

--- src/components/DonorSectionDark4.tsx
+++ src/components/DonorSectionDark4.tsx
@@ -1,8 +1,5 @@
 // Component imports
 import pgIcon from '../assets/images/pg-icon-dark.svg'
-// 1% Partners
-
-// Donors
 import ethereumLogo from '../assets/images/ethereum.svg'
 import baseLogo from '../assets/images/base.svg'
 import optimismLogo from '../assets/images/optimism.svg'
@@ -12,14 +9,6 @@ import shapeLogo from '../assets/images/shape.svg'
 import zksyncLogo from '../assets/images/zksync.svg'
 import zoraLogo from '../assets/images/zora.svg'
 import arbitrumLogo from '../assets/images/arbitrum.svg'
-
-/*
-
-VanEck
-*/
-
-const partners = [
-]
 
 const donors = [
   { name: 'Ethereum', logo: ethereumLogo },
@@ -33,41 +22,18 @@ const donors = [
   { name: 'Zora', logo: zoraLogo },
 ]
 
-
 interface DonorSectionProps {
   className?: string
   theme?: 'dark' | 'light'
-  type?: 'Donors' | 'Partners'
+  type?: 'Donors'
   featured?: boolean
 }
 
 const DonorSection = ({ className = '', theme = 'light', type = 'Donors', featured = false }: DonorSectionProps) => {
-  const logos = type === 'Donors' ? donors : partners
+  const logos = donors;
 
-return (
-  <div className={`donors ${className} pt-4 pb-0`}>
-    
-    {featured && type === 'Partners' ? (
-      <div className="silver-gradient silver-shimmer rounded-xl p-8">
-        <div className="flex flex-wrap justify-center gap-6">
-          {logos.map((donor) => (
-            <a 
-              key={donor.name}
-              href={donor.url}
-              target="_blank"
-              rel="noopener noreferrer"
-              className="flex items-center justify-center h-10 md:h-15"
-            >
-              <img 
-                src={donor.logo} 
-                alt={donor.name}
-                className="h-[60px] w-auto max-w-[158px]"
-              />
-            </a>
-          ))}
-        </div>
-      </div>
-    ) : type === 'Donors' ? (
+  return (
+    <div className={`donors ${className} pt-4 pb-0`}>
       <div className="p-8">
         <div className="flex flex-wrap justify-center gap-6">
           {logos.map((donor) => (
@@ -81,33 +47,14 @@ return (
               <img 
                 src={donor.logo} 
                 alt={donor.name}
-                className="h-[60px] w-auto max-w-[158px]"
+                className="h-[90px] w-auto max-w-[158px]"
               />
             </a>
           ))}
         </div>
       </div>
-    ) : (
-      <div className="donor-logos grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-6 md:gap-8 items-center">
-        {logos.map(donor => (
-          <a 
-            key={donor.name}
-            href={donor.url}
-            target="_blank"
-            rel="noopener noreferrer"
-            className="flex items-center justify-center h-10 md:h-15"
-          >
-            <img 
-              src={theme === 'light' ? donor.logoWhite || donor.logo : donor.logo} 
-              alt={donor.name}
-              className="h-[60px] w-auto max-w-[158px]"
-            />
-          </a>
-        ))}
-      </div>
-    )}
-  </div>
-)
+    </div>
+  )
 }
 
 export default DonorSection
