3c099d46cfb201faec49ea85e4a8e1430e1658f9
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-23 12:30:10 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -121,7 +121,22 @@
           </Grid>
         </Section.Row>
       </Section>
-
+        
+      <Section background="white" divider="var(--brand-primary)" line={true}>
+        <Section.Row align="start" >
+          <Grid columns={12} className="py-16">
+            <Grid.Item span={6} className="flex flex-col gap-6">
+              <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{aboutContent.funders.title}</h2>
+              <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.funders.subtitle1}</h2>
+              <a href={aboutContent.funders.buttonLink} rel="noopener noreferrer" className="!bg-[var(--brand-primary)] !border-none text-[var(--gray-dark)] hover:text-[var(--gray-dark)] !hover:text-[var(--gray-dark)] px-8 py-3 rounded-lg hover:!opacity-80 transition-colors w-fit inline-block no-underline">Fund Our Work</a>
+            </Grid.Item>
+            <Grid.Item span={6} className="flex flex-col gap-6">
+              <DonorSectionDark2 theme="dark" type="Partners" featured={true}/>
+              <DonorSectionDark2 theme="dark" type="Donors" />
+            </Grid.Item>
+          </Grid>
+        </Section.Row>
+      </Section>
 
 
 
