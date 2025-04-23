e5f494bc5160a75eb59ccdbe21763f2e70e446e5
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-23 12:38:08 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -142,18 +142,30 @@
         <Section.Row align="start">
           <Grid columns={12} className="py-16">
             <Grid.Item span={6} className="flex flex-col gap-6 md:h-full">
-
-
-
-
-
-
-
-              
+              <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{aboutContent.impact.title}</h2>
+              <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.impact.subtitle}</h2>
+              <DuneStat 
+                queryId="5021583" 
+                theme="light" 
+                title="Cumulative Years Contributed"
+                suffix="years"
+                variableName="time"
+                type="number"
+              />
+              <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)] flex items-center">
+                <a href="https://protocol-guild.readthedocs.io/en/latest/01-eligibility.html" target="_blank" rel="noopener noreferrer" className="flex items-center">
+                  {aboutContent.impact.link}
+                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="ml-1">
+                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                    <polyline points="15 3 21 3 21 9"></polyline>
+                    <line x1="10" y1="14" x2="21" y2="3"></line>
+                  </svg>
+                </a>
+              </h2>
             </Grid.Item>
             <Grid.Item span={6} className="flex flex-col gap-6">
               <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                {homeContent.impact.areas.map((area, index) => (
+                {aboutContent.impact.areas.map((area, index) => (
                   <FeatureCard
                     key={index}
                     title={area.title}
