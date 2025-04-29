2f2bbd4f6e0aac6cdb4a9e588e6f1a33ed883ee9
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-29 10:42:16 +0200

    Update HeroBar.tsx

--- src/components/HeroBar.tsx
+++ src/components/HeroBar.tsx
@@ -4,6 +4,9 @@ const HeroBar = () => {
       <svg className="w-full h-full" viewBox="0 0 1239 16" fill="none" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none">
         <path d="M261.122 0.775391H329.607L300.241 15.989H231.756L261.122 0.775391Z" fill="#FFD401" style={{fill: 'color(display-p3 1.0000 0.8314 0.0039)'}} />
         <path d="M98.6687 0.775391H261.125L231.759 15.989H69.3027L98.6687 0.775391Z" fill="#FFD401" style={{fill: 'color(display-p3 1.0000 0.8314 0.0039)'}} />
+        <path d="M0.814453 0.775391H98.6653L69.2993 15.989H0.814453L0.814453 0.775391Z" fill="#A89C5D" style={{fill: 'color(display-p3 0.6588 0.6118 0.3647)'}} />
+        <path d="M403.962 0.775391H1238.42V15.989H374.596L403.962 0.775391Z" fill="#E8D5FF" fillOpacity="0.913725" style={{fill: 'color(display-p3 0.9098 0.8353 1.0000)', fillOpacity: '0.913725'}} />
+        <path d="M329.604 0.775391H403.962L374.596 15.989H300.238L329.604 0.775391Z" fill="#FF67EB" style={{fill: 'color(display-p3 1.0000 0.4039 0.9216)'}} />
       </svg>
     </div>
   );
