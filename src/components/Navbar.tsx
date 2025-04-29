6fac6abddcd31dd8ce2c82e3a978f6ee94d74446
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-29 12:13:48 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -77,7 +77,7 @@ const Navbar: FC<NavbarProps> = ({ theme = 'light' }) => {
           })}
         </div>
         <Link to="/" className="md:order-1">
-          <PGLockup fill="var(--gray-dark)" logoColor="#3cbeed" className="h-8 w-auto max-w-[600px]" />
+          <PGLockup fill="var(--gray-dark)" logoColor="#3cbeed" className="h-12 w-auto max-w-[600px]" />
         </Link>
       </div>
     </nav>
