bfe803424191b5b2cb0222a9da86a1bbbe2ac995
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-29 10:34:07 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,11 +14,11 @@ const Navbar: FC<NavbarProps> = ({ theme = 'light' }) => {
 
   return (
     <nav className="bg-white py-6">
-      <div className="flex flex-col items-start md:flex-row gap-8 md:items-center justify-between w-full">
+      <div className="flex justify-between items-center w-full">
         <Link to="/">
           <PGLockup fill="var(--gray-dark)" logoColor="#3cbeed" className="h-8 w-auto max-w-[200px]" />
         </Link>
-        <div className="flex flex-col md:flex-row gap-6 md:gap-8 md:ml-auto md:items-center">
+        <div className="flex md:ml-auto gap-6 md:gap-8">
           {links.map(link => (
             link.target === '_blank' ? (
               <a
