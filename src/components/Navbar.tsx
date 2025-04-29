caac7743809be5665f3de7f94c4320569aaa95ce
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-29 10:25:28 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,34 +13,63 @@ const Navbar: FC<NavbarProps> = ({ theme = 'light' }) => {
   const { main: links } = navigation;
 
   return (
-    <nav className="bg-white py-6">
-      <div className="flex flex-col items-start md:flex-row gap-8 md:items-center justify-between w-full">
-        <Link to="/">
-          <PGLockup fill="var(--gray-dark)" logoColor="#3cbeed" className="h-8 w-auto max-w-[200px]" />
-        </Link>
-        <div className="flex flex-col items-start md:flex-row gap-6 md:gap-8 md:items-center">
-          {links.map(link => (
-            link.target === '_blank' ? (
-              <a
-                key={link.path}
-                href={link.path}
-                target="_blank"
-                rel="noopener noreferrer"
-                className={`text-sm font-semibold text-[var(--gray-dark)] hover:opacity-80 transition-opacity`}
-              >
-                {link.name}
-              </a>
-            ) : (
-              <Link
-                key={link.path}
-                to={link.path}
-                className={`text-sm font-semibold text-[var(--gray-dark)] hover:opacity-80 transition-opacity`}
-              >
-                {link.name}
-              </Link>
-            )
-          ))}
-        </div>
+    <nav className="flex flex-row items-center justify-between py-6">
+      <Link to="/">
+        <PGLockup fill="var(--gray-dark)" logoColor="#3cbeed" className="h-8 w-auto max-w-[200px]" />
+      </Link>
+      <div className="flex flex-row items-center gap-8">
+        {links.map(link => (
+          link.target === '_blank' ? (
+            <a
+              key={link.path}
+              href={link.path}
+              target="_blank"
+              rel="noopener noreferrer"
+              className={`font-medium text-sm transition-opacity flex items-center gap-1 ${
+                currentPath === link.path
+                  ? theme === 'light' 
+                    ? 'text-[var(--brand-primary)]'
+                    : theme === 'brand'
+                      ? 'text-[var(--bg-white)] hover:!text-[var(--bg-white)] hover:!opacity-50'
+                      : 'text-[var(--brand-primary)] hover:!text-[var(--brand-primary)] hover:!opacity-50'
+                  : theme === 'light'
+                    ? 'text-[#212121] hover:text-[var(--gray-dark)]'
+                    : theme === 'brand'
+                      ? 'text-[var(--gray-dark)] hover:!text-[var(--gray-dark)] hover:!opacity-50'
+                      : 'text-[var(--bg-white)] hover:!text-[var(--bg-white)] hover:!opacity-50'
+              }`}
+            >
+              {link.name}
+              {link.target === '_blank' && (
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="mb-[2px]">
+                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                  <polyline points="15 3 21 3 21 9"></polyline>
+                  <line x1="10" y1="14" x2="21" y2="3"></line>
+                </svg>
+              )}
+            </a>
+          ) : (
+            <Link
+              key={link.path}
+              to={link.path}
+              className={`font-medium text-sm transition-opacity flex items-center gap-1 ${
+                currentPath === link.path
+                  ? theme === 'light' 
+                    ? 'text-[var(--brand-primary)]'
+                    : theme === 'brand'
+                      ? 'text-[var(--bg-white)] hover:!text-[var(--bg-white)] hover:!opacity-50'
+                      : 'text-[var(--brand-primary)] hover:!text-[var(--brand-primary)] hover:!opacity-50'
+                  : theme === 'light'
+                    ? 'text-[#212121] hover:text-[var(--gray-dark)]'
+                    : theme === 'brand'
+                      ? 'text-[var(--gray-dark)] hover:!text-[var(--gray-dark)] hover:!opacity-50'
+                      : 'text-[var(--bg-white)] hover:!text-[var(--bg-white)] hover:!opacity-50'
+              }`}
+            >
+              {link.name}
+            </Link>
+          )
+        ))}
       </div>
     </nav>
   );
