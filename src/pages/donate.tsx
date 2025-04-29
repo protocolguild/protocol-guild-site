345ca6052093256e724f9f5704afc92672848ed2
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-29 16:03:15 +0200

    Update donate.tsx

--- src/pages/donate.tsx
+++ src/pages/donate.tsx
@@ -235,7 +235,7 @@ const Donate: FC = () => {
                   />
                 </div>
               </div>
-              <DonorSectionDark4 theme="dark" type="Donors" />
+              <DonorSectionDark4 theme="dark" type="Donors" className="max-w-[100%] lg:max-w-[70%]" />
             </Grid.Item>
           </Grid>
         </Section.Row>
