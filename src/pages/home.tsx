398aa4139b1ddbd0924913e22a3159283206616c
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-29 15:04:45 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -17,7 +17,7 @@ const [currentQuote, setCurrentQuote] = useState(0);
 
 const nextQuote = () => {
   setCurrentQuote((prev) => (prev + 1) % homeContent.quotes.length);
-};
+};f
 
 const previousQuote = () => {
   setCurrentQuote((prev) => (prev - 1 + homeContent.quotes.length) % homeContent.quotes.length);
@@ -120,7 +120,7 @@ return (
           <Grid.Item span={6} className="flex flex-col gap-6">
             <h2 className="text-3xl font-bold leading-[1.1] text-[var(--gray-dark)]">{homeContent.section3.title}</h2>
             <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">{homeContent.section3.subtitle1}</h2>
-            <a href={homeContent.section3.buttonLink} rel="noopener noreferrer" className="!bg-[var(--brand-primary)] !border-none text-[var(--gray-dark)] hover:text-[var(--gray-dark)] !hover:text-[var(--gray-dark)] px-8 py-3 rounded-lg hover:!opacity-80 transition-colors w-fit inline-block no-underline">Fund Our Work</a>
+            <a href={homeContent.section3.buttonLink} rel="noopener noreferrer" className="!bg-[var(--brand-primary)] !border-none text-[var(--gray-dark)] hover:text-[var(--gray-dark)] !hover:text-[var(--gray-dark)] px-8 py-3 rounded-lg hover:!opacity-80 transition-colors w-fit inline-block no-underline">Fund our Work</a>
           </Grid.Item>
           <Grid.Item span={6} className="flex flex-col gap-6">
             <DonorSectionDark2 theme="dark" type="Partners" featured={true}/>
