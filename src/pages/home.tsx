87767627304019fd8f3387d0aec1824dad0072e9
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 09:28:03 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -31,6 +31,12 @@ return (
   <main>
     
     <Section background="white" className="flex flex-col pb-8 md:pb-16 mt-0 relative overflow-hidden">
+      <img
+        src={chainsImage}
+        alt="Ethereum chains"
+        className="absolute top-0 right-0 h-full w-auto object-cover object-right z-0 hidden md:block"
+        style={{ maxWidth: 'none' }}
+      />
       <Section.Row>
         <Grid>
           <Grid.Item span={12} className="flex flex-col">
