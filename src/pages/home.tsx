fb5272ca6fbdb98d8e10605f6de99edd76d8748b
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 12:34:35 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -49,13 +49,11 @@ return (
           </Grid.Item>
         </Grid>
         
-        <Grid className="absolute top-80 left-0 w-full z-10 flex flex-col items-start gap-6 ">
+        <Grid className="absolute top-60 left-0 w-full z-10 flex flex-col items-start gap-6 ">
           <Grid.Item span={12} className="w-full md:w-[65%] gap-6">
-            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.1] tracking-[-1.44px] text-[var(--gray-dark)]">{homeContent.hero.title}</h1>
-            <br/><br/>
-            <p className="text-lg leading-[23px] text-[var(--gray-mid)]">{homeContent.hero.subtitle1}</p>
-            <br/><br/>
-            <p className="text-lg leading-[23px] text-[var(--gray-mid)]">{homeContent.hero.subtitle2}</p>
+            <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.1] tracking-[-1.44px] text-[var(--gray-dark)] mb-4">{homeContent.hero.title}</h1>
+            <p className="text-lg leading-[23px] text-[var(--gray-mid)] mb-4">{homeContent.hero.subtitle1}</p>
+            <p className="text-lg leading-[23px] text-[var(--gray-mid)] mb-4">{homeContent.hero.subtitle2}</p>
           </Grid.Item>
         </Grid>
         
