cdaebd7b6f0c67fa38ba045d37e21eb2ce0aa670
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 12:48:09 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -49,7 +49,7 @@ return (
           </Grid.Item>
         </Grid>
         
-        <Grid className="absolute top-[30px] md:top-[60px] left-0 w-full z-10 flex flex-col items-start">
+        <Grid className="absolute top-[150px] md:top-[100px] left-0 w-full z-10 flex flex-col items-start">
           <Grid.Item span={12} className="w-full md:w-[65%]">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.1] tracking-[-1.44px] text-[var(--gray-dark)] mb-4">{homeContent.hero.title}</h1>
             <p className="text-lg leading-[23px] text-[var(--gray-mid)] mb-4">{homeContent.hero.subtitle1}</p>
