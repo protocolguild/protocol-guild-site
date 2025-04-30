5a6687431cccb0b5e6457b2b7d099db6bfec8037
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 14:08:28 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -32,10 +32,8 @@ return (
     
 <Section background="white" className="flex flex-col relative overflow-hidden">
   <Section.Row className="relative z-1">
-    
     <Grid>
       <Grid.Item span={12} className="relative flex">
-        
         {/* Image container */}
         <div className="absolute top-0 left-0 w-full z-0 flex justify-end h-full">
           <img
@@ -44,21 +42,18 @@ return (
             className="h-full w-auto max-w-full object-contain opacity-0 md:opacity-100"
           />
         </div>
-        
         {/* Content container */}
-        <div className="relative z-10 flex flex-col items-start pt-[200px] pb-[150px] w-full md:w-[65%]">
+        <div className="relative z-10 flex flex-col items-start pt-[200px] pb-[130px] w-full md:w-[65%]">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.1] tracking-[-1.44px] text-[var(--gray-dark)] mb-4">{homeContent.hero.title}</h1>
             <p className="text-lg leading-[23px] text-[var(--gray-mid)] mb-4">{homeContent.hero.subtitle1}</p>
             <p className="text-lg leading-[23px] text-[var(--gray-mid)] mb-4">{homeContent.hero.subtitle2}</p>
         </div>
-        
         {/* Navbar */}
         <Grid className="absolute top-0 left-0 w-full z-10">
           <Grid.Item span={12} className="w-full md:w-[65%]">
             <Navbar theme="light" />
           </Grid.Item>
         </Grid>
-        
       </Grid.Item>
     </Grid>
   </Section.Row>
