0f1839dc8c4285eb5f0ba9f4b676a79762e0cc59
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 08:56:42 +0200

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,7 @@ const { main: links } = navigation;
 return (
   <nav className="bg-white py-6">
     <div className="flex flex-col md:flex-row justify-between w-full">
-      <Link to="/" className="md:order-1">
+      <Link to="/" className="md:order-1 mb-4 md:mb-0">
         <PGLockup fill="var(--gray-dark)" logoColor="#3cbeed" className="h-8 w-auto max-w-[200px]" />
       </Link>
       <div className="flex gap-6 md:gap-8 mb-4 md:mb-0 md:order-2">
