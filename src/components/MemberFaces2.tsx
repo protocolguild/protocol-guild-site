d2c14842a803eec1311574bc237feda642957e5c
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 18:08:00 +0200

    Update MemberFaces2.tsx

--- src/components/MemberFaces2.tsx
+++ src/components/MemberFaces2.tsx
@@ -20,20 +20,19 @@ const MemberFaces: FC<MemberFacesProps> = () => {
     setShuffledMembers(shuffleArray(members));
   }, []);
 
-  const quarterLength = Math.ceil(shuffledMembers.length / 4);
-  const firstQuarter = shuffledMembers.slice(0, quarterLength);
-  const secondQuarter = shuffledMembers.slice(quarterLength, quarterLength * 2);
-  const thirdQuarter = shuffledMembers.slice(quarterLength * 2, quarterLength * 3);
-  const fourthQuarter = shuffledMembers.slice(quarterLength * 3);
+  const ninthLength = Math.ceil(shuffledMembers.length / 9);
+  const rows = Array.from({ length: 9 }, (_, index) => 
+    shuffledMembers.slice(index * ninthLength, (index + 1) * ninthLength)
+  );
 
   return (
     <div className="relative w-full h-auto overflow-hidden flex flex-col gap-2 group mt-3">
-      {[firstQuarter, secondQuarter, thirdQuarter, fourthQuarter].map((quarter, rowIndex) => (
+      {rows.map((row, rowIndex) => (
         <div key={`row-${rowIndex}`} className="relative h-[32px] overflow-hidden flex items-center">
           <div className="absolute h-full left-0 bg-gradient-to-r from-[var(--white)] via-[var(--white)] via-[20%] via-opacity-100 to-opacity-0 z-10 pointer-events-none w-[10%]" />
           <div className="absolute h-full right-0 bg-gradient-to-l from-[var(--white)] via-[var(--white)] via-[20%] via-opacity-100 to-opacity-0 z-10 pointer-events-none w-[10%]" />
           <div className={`flex whitespace-nowrap min-w-max ${rowIndex % 2 === 0 ? styles.scroll : styles['scroll-reverse']} group-hover:animate-pause`}>
-            {quarter.map((member, index) => (
+            {row.map((member, index) => (
               <a
                 href={member.link}
                 target="_blank"
