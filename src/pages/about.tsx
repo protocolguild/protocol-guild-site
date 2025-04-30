c7d50ee5f22f7a36eeddf3dcc72ab1dc2afcb48b
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 09:07:36 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -53,7 +53,12 @@ return (
             <h2 className="col-span-3 text-3xl font-bold leading-[1.1] text-[var(--gray-dark)] mb-4">
               {aboutContent.organization.title}
             </h2>
-            <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.organization.description}</h2>
+            <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.organization.description1}</h2>
+            <br/><br/>
+            <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.organization.description2}</h2>
+            <br/><br/>
+            <h2 className="!text-lg mt-5 !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.organization.description3}</h2>
+            <a href="https://calendly.com/vecchiarelli-peter/30min" target="_blank" rel="noopener noreferrer" className="!bg-[var(--brand-primary)] !border-none text-[var(--gray-dark)] hover:text-[var(--gray-dark)] !hover:text-[var(--gray-dark)] px-8 py-3 rounded-lg hover:!opacity-80 transition-colors w-fit inline-block no-underline">Schedule Call</a>
           </Grid.Item>
           <Grid.Item span={6} className="flex flex-col gap-4">
             <div className="grid grid-cols-1 gap-6">
