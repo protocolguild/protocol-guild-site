7bbf00d9237d28f618bbc08e71095cbd998a80cb
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 10:27:59 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -44,7 +44,7 @@ return (
           </Grid.Item>
         </Grid>
       </Section.Row>
-      <Section.Row className="flex-1 flex items-center justify-center relative z-1 mt-8 mb-8 pt-8 pb-8">
+      <Section.Row className="flex-1 flex items-center justify-center relative z-1 mt-16 mb-16 pt-16 pb-16">
         <Grid>
           <Grid.Item span={8} className="flex flex-col gap-6">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.1] tracking-[-1.44px] text-[var(--gray-dark)]">
