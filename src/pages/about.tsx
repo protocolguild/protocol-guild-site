fdb333cbfb7517310df9e84c8b95ba8e41c7e3a2
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 09:32:01 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -30,7 +30,7 @@ return (
       <img
         src={chainsImage}
         alt="Ethereum chains"
-        className="absolute top-0 right-0 h-full w-[calc(100% - 3rem)] object-cover object-right z-0 hidden md:block"
+        className="absolute top-0 right-0 h-full w-auto object-cover object-right z-0 hidden md:block"
         style={{ maxWidth: 'none' }}
       />
       <Section.Row>
