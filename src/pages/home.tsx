c03b5f29f84fca9053abe9233464e6b951246756
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 10:10:07 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -30,7 +30,7 @@ const goToQuote = (index: number) => {
 return (
   <main>
     
-    <Section background="white" className="flex flex-col pb-8 md:pb-16 mt-0 relative overflow-hidden">
+    <Section background="white" className="flex flex-col relative overflow-hidden">
       <img
         src={chainsImage}
         alt="Ethereum chains"
@@ -44,7 +44,7 @@ return (
           </Grid.Item>
         </Grid>
       </Section.Row>
-      <Section.Row className="flex-1 flex items-center justify-center relative z-1 mt-15 mb-15">
+      <Section.Row className="flex-1 flex items-center justify-center relative z-1 mt-30 mb-30">
         <Grid>
           <Grid.Item span={8} className="flex flex-col gap-6">
             <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.1] tracking-[-1.44px] text-[var(--gray-dark)]">
