3a872780a8d2ee2208c695ceec3f6f5fdbb7daa5
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 09:34:38 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -31,12 +31,14 @@ return (
   <main>
     
     <Section background="white" className="flex flex-col pb-8 md:pb-16 mt-0 relative overflow-hidden">
-      <img
-        src={chainsImage}
-        alt="Ethereum chains"
-        className="absolute top-0 right-0 h-full w-auto object-cover object-right z-0 hidden md:block"
-        style={{ maxWidth: 'none' }}
-      />
+      <div className="relative w-full md:w-8/12"> {/* Adjust width as needed */}
+        <img
+          src={chainsImage}
+          alt="Ethereum chains"
+          className="h-full w-auto object-cover object-right z-0 hidden md:block"
+          style={{ maxWidth: '100%' }} // Ensure the image does not exceed the container width
+        />
+      </div>
       <Section.Row>
         <Grid>
           <Grid.Item span={12} className="flex flex-col">
