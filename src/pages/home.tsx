d7c39317f55e48bf292e9bfdb66f913ffe169f7c
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 11:38:10 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -37,8 +37,7 @@ return (
         <img
           src={chainsImage}
           alt="Ethereum chains"
-          className="h-full w-auto object-cover object-right z-0 hidden md:block"
-          style={{ maxWidth: 'none', maxHeight: '700px' }}
+          className="h-auto max-h-[700px] w-auto object-cover object-right z-0 hidden md:block"
         />            
       </Grid.Item>
     </Grid>
