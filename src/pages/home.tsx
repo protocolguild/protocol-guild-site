aba7a5448e69d4d0bb38c1bbbf60271450aeedd4
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 12:09:39 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -30,31 +30,42 @@ const goToQuote = (index: number) => {
 return (
   <main>
     
-<Section background="white" className="flex flex-col relative overflow-hidden">
-  <Section.Row className="relative z-1">
-    <Grid>
-      <Grid.Item span={12} className="relative"> {/* Parent grid item for the image */}
-        <div className="flex justify-end"> {/* Flex container to align the image */}
-          <img
-            src={chainsImage}
-            alt="Ethereum chains"
-            className="h-auto max-h-[700px] w-auto object-contain z-0 bg-white opacity-0 md:opacity-100" // Image in the background
-          />
-        </div>
-        <Grid className="absolute top-0 left-0 w-full z-10"> {/* Nested grid for the navbar */}
-          <Grid.Item span={12}>
-            <Navbar theme="light" /> {/* Navbar inside the nested grid */}
+    <Section background="white" className="flex flex-col relative overflow-hidden">
+      <Section.Row className="relative z-1">
+        
+        <Grid>
+          <Grid.Item span={12} className="relative">
+            <div className="flex justify-end">
+              <img
+                src={chainsImage}
+                alt="Ethereum chains"
+                className="h-auto max-h-[700px] w-auto object-contain z-0 opacity-100 md:opacity-100 transition-opacity duration-300 ease-in-out"
+              />
+            </div>
+            
+            <Grid className="absolute top-0 left-0 w-full z-10">
+              <Grid.Item span={12}>
+                <Navbar theme="light" />
+              </Grid.Item>
+            </Grid>
+            
+            <Grid className="absolute top-20 left-0 w-full z-10 text-center"> {/* Adjust top positioning as needed */}
+              <Grid.Item span={8}}>
+                <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.1] tracking-[-1.44px] text-[var(--gray-dark)]">
+                  {homeContent.hero.title}
+                </h1>
+                <p className="text-lg leading-[23px] text-[var(--gray-mid)]">
+                  {homeContent.hero.subtitle1}
+                  <br/><br/>
+                  {homeContent.hero.subtitle2}
+                </p>
+              </Grid.Item>
+            </Grid>
+            
           </Grid.Item>
         </Grid>
-      </Grid.Item>
-    </Grid>
-  </Section.Row>
-</Section>
-
-
-
-
-
+      </Section.Row>
+    </Section>
 
     <Section background="white" divider="var(--brand-primary)" line={true}>
       <Section.Row align="start">
