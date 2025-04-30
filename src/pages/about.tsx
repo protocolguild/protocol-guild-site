40a9bbbcca69dbee9914e6ebd3969d65537b425b
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 17:46:55 +0200

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -60,6 +60,64 @@ return (
             <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.organization.description2}</h2>
             <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)]">{aboutContent.organization.description3}</h2>
             <a href="https://calendly.com/vecchiarelli-peter/30min" target="_blank" rel="noopener noreferrer" className="!bg-[var(--brand-primary)] !border-none text-[var(--gray-dark)] hover:text-[var(--gray-dark)] !hover:text-[var(--gray-dark)] px-8 py-3 rounded-lg hover:!opacity-80 transition-colors w-fit inline-block no-underline">Schedule Call</a>
+            <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)] flex items-center">
+              <a href="https://protocol-guild.readthedocs.io/en/latest/02-membership.html" target="_blank" rel="noopener noreferrer" className="flex items-center">
+                {aboutContent.organization.link}
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="ml-1">
+                  <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                  <polyline points="15 3 21 3 21 9"></polyline>
+                  <line x1="10" y1="14" x2="21" y2="3"></line>
+                </svg>
+              </a>
+            </h2>
+          </div>
+          <br />
+          <div>
+            <h3 className="text-lg font-bold leading-[1.1] text-[var(--gray-dark)] mb-4">{aboutContent.organization.team}</h3>
+            <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
+              <div className="flex flex-col items-start">
+                <img src={trentImg} alt="Trent Van Epps" className="w-full aspect-square object-cover" />
+                <div className="mt-2 flex flex-col items-start w-full gap-1">
+                  <p className="text-sm font-bold">Trent Van Epps</p>
+                  <a href="https://x.com/trent_vanepps" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)] text-sm font-normal">@trent_vanepps
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block ml-1">
+                      <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                      <polyline points="15 3 21 3 21 9"></polyline>
+                      <line x1="10" y1="14" x2="21" y2="3"></line>
+                    </svg>
+                  </a>
+                  <p className="text-xs">Strategy, fundraising, mechanism design</p>
+                </div>
+              </div>
+              <div className="flex flex-col items-start">
+                <img src={cheekyImg} alt="Cheeky Gorilla" className="w-full aspect-square object-cover" />
+                <div className="mt-2 flex flex-col items-start w-full gap-1">
+                  <p className="text-sm font-bold">Cheeky Gorilla</p>
+                  <a href="https://x.com/cheekygorilla0x" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)] text-sm font-normal">@cheekygorilla0x
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block ml-1">
+                      <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                      <polyline points="15 3 21 3 21 9"></polyline>
+                      <line x1="10" y1="14" x2="21" y2="3"></line>
+                    </svg>
+                  </a>
+                  <p className="text-xs">Internal operations, legal, fundraising</p>
+                </div>
+                <div className="flex flex-col items-start">
+                  <img src={peterImg} alt="Peter Vecchiarelli" className="w-full aspect-square object-cover" />
+                  <div className="mt-2 flex flex-col items-start w-full gap-1">
+                    <p className="text-sm font-bold">Peter Vecchiarelli</p>
+                    <a href="https://x.com/AlphaLemonade" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)] text-sm font-normal">@AlphaLemonade
+                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block ml-1">
+                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
+                        <polyline points="15 3 21 3 21 9"></polyline>
+                        <line x1="10" y1="14" x2="21" y2="3"></line>
+                      </svg>
+                    </a>
+                    <p className="text-xs">Fundraising, communications</p>
+                  </div>
+                </div>
+              </div>
+            </div>
           </Grid.Item>
           <Grid.Item span={6} className="flex flex-col gap-4">
             <div className="grid grid-cols-1 gap-6">
@@ -75,64 +133,6 @@ return (
                 yAxisKey="total_count"
                 formatDate
               />
-              <h2 className="!text-lg !font-normal !leading-[24px] !text-[var(--gray-dark)] flex items-center">
-                <a href="https://protocol-guild.readthedocs.io/en/latest/02-membership.html" target="_blank" rel="noopener noreferrer" className="flex items-center">
-                  {aboutContent.organization.link}
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="ml-1">
-                    <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                    <polyline points="15 3 21 3 21 9"></polyline>
-                    <line x1="10" y1="14" x2="21" y2="3"></line>
-                  </svg>
-                </a>
-              </h2>
-            </div>
-            <br />
-            <div>
-              <h3 className="text-lg font-bold leading-[1.1] text-[var(--gray-dark)] mb-4">{aboutContent.organization.team}</h3>
-              <div className="grid grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <div className="flex flex-col items-start">
-                  <img src={trentImg} alt="Trent Van Epps" className="w-full aspect-square object-cover" />
-                  <div className="mt-2 flex flex-col items-start w-full gap-1">
-                    <p className="text-sm font-bold">Trent Van Epps</p>
-                    <a href="https://x.com/trent_vanepps" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)] text-sm font-normal">@trent_vanepps
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block ml-1">
-                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                        <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                      </svg>
-                    </a>
-                    <p className="text-xs">Strategy, fundraising, internal gov.</p>
-                  </div>
-                </div>
-                <div className="flex flex-col items-start">
-                  <img src={cheekyImg} alt="Cheeky Gorilla" className="w-full aspect-square object-cover" />
-                  <div className="mt-2 flex flex-col items-start w-full gap-1">
-                    <p className="text-sm font-bold">Cheeky Gorilla</p>
-                    <a href="https://x.com/cheekygorilla0x" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)] text-sm font-normal">@cheekygorilla0x
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block ml-1">
-                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                        <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                      </svg>
-                    </a>
-                    <p className="text-xs">Internal ops, legal, fundraising</p>
-                  </div>
-                </div>
-                <div className="flex flex-col items-start">
-                  <img src={peterImg} alt="Peter Vecchiarelli" className="w-full aspect-square object-cover" />
-                  <div className="mt-2 flex flex-col items-start w-full gap-1">
-                    <p className="text-sm font-bold">Peter Vecchiarelli</p>
-                    <a href="https://x.com/AlphaLemonade" target="_blank" rel="noopener noreferrer" className="text-[var(--gray-dark)] text-sm font-normal">@AlphaLemonade
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="inline-block ml-1">
-                        <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
-                        <polyline points="15 3 21 3 21 9"></polyline>
-                        <line x1="10" y1="14" x2="21" y2="3"></line>
-                      </svg>
-                    </a>
-                    <p className="text-xs">Fundraising, comms</p>
-                  </div>
-                </div>
-              </div>
             </div>
           </Grid.Item>
         </Grid>
