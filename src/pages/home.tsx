291ec3e75080c92daaef03a55fed0540c2ed7597
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-04-30 13:17:37 +0200

    Update home.tsx

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -33,7 +33,7 @@ return (
     <Section background="white" className="flex flex-col relative overflow-hidden">
       <Section.Row className="relative z-1">
         
-        <Grid className="relative">
+        <Grid>
           <Grid.Item span={12} className="relative">
             <div className="flex justify-end">
               <img
@@ -49,13 +49,13 @@ return (
               </Grid.Item>
             </Grid>
             
-            <div className="absolute top-0 left-0 w-full z-10 h-[400px] flex items-center">
-              <div className="w-full md:w-[65%] mx-auto">
+            <Grid className="absolute top-[200px] md:top-[200px] left-0 w-full z-10 flex flex-col items-start">
+              <Grid.Item span={12} className="w-full md:w-[65%]">
                 <h1 className="text-3xl md:text-4xl lg:text-5xl font-bold leading-[1.1] tracking-[-1.44px] text-[var(--gray-dark)] mb-4">{homeContent.hero.title}</h1>
                 <p className="text-lg leading-[23px] text-[var(--gray-mid)] mb-4">{homeContent.hero.subtitle1}</p>
                 <p className="text-lg leading-[23px] text-[var(--gray-mid)] mb-4">{homeContent.hero.subtitle2}</p>
-              </div>
-            </div>
+              </Grid.Item>
+            </Grid>
             
           </Grid.Item>
         </Grid>
