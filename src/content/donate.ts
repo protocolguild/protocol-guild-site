b7c5200ecad593fbfee26e77ae89be075560b9a0
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2025-05-13 08:08:22 +0200

    Update donate.ts

--- src/content/donate.ts
+++ src/content/donate.ts
@@ -42,19 +42,19 @@ export const donateContent = {
   },
   quotes: [
     {
-      text: "The 1% allocation is our way of giving back to the core Ethereum maintainer community and acknowledging their exceptional work over the years.",
+      text: "\"The 1% allocation is our way of giving back to the core Ethereum maintainer community and acknowledging their exceptional work over the years.\"",
       author: "Taiko, 1% Pledge Partner"
     },
     {
-      text: "Thank you, Ethereum contributors, for nearly a decade of relentless building & ongoing stewardship of this common infrastructure.",
+      text: "\"Thank you, Ethereum contributors, for nearly a decade of relentless building & ongoing stewardship of this common infrastructure.\"",
       author: "VanEck, Donor"
     },
     {
-      text: "It is in the interest of our community to help ensure the stability of the Ethereum L1 R&D contributor set.",
+      text: "\"It is in the interest of our community to help ensure the stability of the Ethereum L1 R&D contributor set.\"",
       author: "INTMAX, 1% Pledge Partner"
     },
     {
-      text: "We wouldn’t be here without Ethereum and its core developers. Ethereum is the world computer and one of the most important innovations of our time.",
+      text: "\"We wouldn’t be here without Ethereum and its core developers. Ethereum is the world computer and one of the most important innovations of our time.\"",
       author: "Lambda Class, 1% Pledge Partner"
     }
   ],
