f015f54e107631e5fc1ee8773962b63b19350a5c
bendrsio <benjy.desprets@gmail.com>
2025-08-14 17:54:38 -0400

    Update Blog Post

--- src/pages/blog-post.tsx
+++ src/pages/blog-post.tsx
@@ -18,18 +18,25 @@ const BlogPost: FC = () => {
       return null
     }
   }, [slug])
-
+  const formattedDate = post
+    ? new Date(post.date).toLocaleDateString('en-US', {
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric',
+      })
+    : null
   return (
     <main>
       <StandaloneNavbar />
 
-      <Section background="white" divider="var(--brand-primary)" line={true}>
+      <Section background="white">
         <Section.Row align="start">
           <Grid columns={12} className="py-16">
             <Grid.Item span={12} className="flex flex-col gap-6">
               {post ? (
                 <article className="prose prose-neutral max-w-none">
-                  <ReactMarkdown>{post.content}</ReactMarkdown>
+                  <h1>{post.title}</h1>
+                  <h2>{formattedDate}</h2>
                 </article>
               ) : (
                 <p className="text-[var(--gray-dark)]">
@@ -40,6 +47,19 @@ const BlogPost: FC = () => {
           </Grid>
         </Section.Row>
       </Section>
+      {post ? (
+        <Section background="white" divider="var(--brand-primary)" line={true}>
+          <Section.Row align="start">
+            <Grid columns={12} className="py-16">
+              <Grid.Item span={12} className="flex flex-col gap-6">
+                <article className="prose prose-neutral max-w-none">
+                  <ReactMarkdown>{post.content}</ReactMarkdown>
+                </article>
+              </Grid.Item>
+            </Grid>
+          </Section.Row>
+        </Section>
+      ) : null}
     </main>
   )
 }
