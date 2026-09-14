600881356f589757b05950e4c2e0d822df6f1621
cheeky-gorilla <76262359+cheeky-gorilla@users.noreply.github.com>
2026-09-14 17:58:00 +0000

    Remove Megapot banner

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,7 +1,5 @@
 import { FC, ReactNode } from 'react'
 import Footer from './Footer'
-import AnnouncementBanner from './AnnouncementBanner'
-
 interface LayoutProps {
   children: ReactNode
 }
@@ -9,7 +7,6 @@ interface LayoutProps {
 const Layout: FC<LayoutProps> = ({ children }) => {
   return (
     <div className="app min-h-screen bg-white">
-      <AnnouncementBanner />
       {children}
       <Footer />
     </div>
